5f1b34d61f1e18039303876472559407b65a3ed4
vishnu <vishnuvanced23@gmail.com>
2023-12-30 17:07:59 +0530

    adding charts and styling

--- src/components/stockList.js
+++ src/components/stockList.js
@@ -1,7 +1,9 @@
 // src/components/StockList.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './StockList.css'; 
+import "chart.js/auto";
+import { Line } from 'react-chartjs-2';
+import './StockList.css';
 
 const StockList = () => {
   const [stocks, setStocks] = useState([]);
@@ -13,9 +15,9 @@ const StockList = () => {
           'https://cloud.iexapis.com/stable/stock/market/batch',
           {
             params: {
-              token: 'pk_2e919cd3256d400ca1250a71fd8d94fe', 
-              symbols: 'AAPL,GOOGL,MSFT,AMZN,FB,TSLA,NFLX,V,DIS,JPM', 
-              types: 'quote',
+              token: 'pk_2e919cd3256d400ca1250a71fd8d94fe',
+              symbols: 'AAPL,GOOGL,MSFT,AMZN,FB,TSLA,NFLX,V,DIS,JPM',
+              types: 'quote,chart',
             },
           }
         );
@@ -23,9 +25,8 @@ const StockList = () => {
         const stockData = Object.values(response.data);
         const stocksWithAllTimeHighsAndLows = await Promise.all(
           stockData.map(async (stock) => {
-            const allTimeHigh = await getAllTimeHigh(stock.quote.symbol);
-            const allTimeLow = await getAllTimeLow(stock.quote.symbol);
-            return { ...stock, allTimeHigh, allTimeLow };
+            const historicalData = await getHistoricalData(stock.quote.symbol);
+            return { ...stock, historicalData };
           })
         );
 
@@ -38,58 +39,77 @@ const StockList = () => {
     fetchStocks();
   }, []);
 
-  const getAllTimeHigh = async (symbol) => {
+  const getHistoricalData = async (symbol) => {
     try {
       const response = await axios.get(
-        `https://cloud.iexapis.com/stable/stock/${symbol}/stats`,
+        `https://cloud.iexapis.com/stable/stock/${symbol}/chart/1y`,
         {
           params: {
-            token: 'pk_2e919cd3256d400ca1250a71fd8d94fe', 
+            token: 'pk_2e919cd3256d400ca1250a71fd8d94fe',
           },
         }
       );
-      return response.data.week52high;
+      return response.data;
     } catch (error) {
-      console.error(`Error fetching all-time high for ${symbol}:`, error.message);
-      return null;
-    }
-  };
-
-  const getAllTimeLow = async (symbol) => {
-    try {
-      const response = await axios.get(
-        `https://cloud.iexapis.com/stable/stock/${symbol}/stats`,
-        {
-          params: {
-            token: 'pk_2e919cd3256d400ca1250a71fd8d94fe', 
-          },
-        }
-      );
-      return response.data.week52low;
-    } catch (error) {
-      console.error(`Error fetching all-time low for ${symbol}:`, error.message);
-      return null;
+      console.error(`Error fetching historical data for ${symbol}:`, error.message);
+      return [];
     }
   };
 
   return (
     <div className="stock-list-container">
       <h2>Stock Market App</h2>
-      <ul className="stock-list">
+      <div className="stock-list">
         {stocks.map((stock) => (
-          <li key={stock.quote.symbol} className="stock-item">
+          <div key={stock.quote.symbol} className="stock-item">
             <span className="stock-symbol">{stock.quote.symbol}</span>
             <div className="stock-info">
               <span className="stock-price">
                 Latest Price: ${stock.quote.latestPrice.toFixed(2)}
               </span>
-              <span className="stock-high-low">
-                All-Time High: ${stock.allTimeHigh} | All-Time Low: ${stock.allTimeLow}
-              </span>
             </div>
-          </li>
+            <div className="stock-chart">
+              <Line
+                data={{
+                  labels: stock.historicalData.map((data) => data.label),
+                  datasets: [
+                    {
+                      label: 'Stock Price',
+                      data: stock.historicalData.map((data) => data.close),
+                      borderColor: 'rgba(75,192,192,1)',
+                      borderWidth: 2,
+                      fill: false,
+                    },
+                  ],
+                }}
+                options={{
+                  scales: {
+                    xAxes: [
+                      {
+                        type: 'time',
+                        time: {
+                          unit: 'day',
+                        },
+                      },
+                    ],
+                    yAxes: [
+                      {
+                        ticks: {
+                          beginAtZero: true,
+                        },
+                        scaleLabel: {
+                          display: true,
+                          labelString: 'Stock Price (USD)',
+                        },
+                      },
+                    ],
+                  },
+                }}
+              />
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
